5109cbaa31db9b789e57cf52eceb7f406c1a8e43
Sarvar Jafarov <sarvar.jafarov@gmail.com>
2025-12-11 16:37:16 -0500

    Add SSL support for production database connections

--- src/config/database.js
+++ src/config/database.js
@@ -10,6 +10,7 @@ const pool = new Pool({
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
+  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
 });
 
 // Test connection
